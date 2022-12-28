5bcd41b130cbcf02bd50a9b4390dc2ec7adceeb5
sonu-ml <77959887+sonu-ml@users.noreply.github.com>
2022-12-28 23:37:33 +0530

    Update index.tsx

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -9,14 +9,13 @@ interface propTypes {
 const Header = ({ className, activeTab }: propTypes) => {
   const [showHeaderDropDown, setShowHeaderDropDown] = useState(false);
   <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-252815904-1"></script>
-<script>
+  async src="https://www.googletagmanager.com/gtag/js?id=UA-252815904-1"
+  
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'UA-252815904-1');
-</script>
 
   return (
     <header className={className}>
